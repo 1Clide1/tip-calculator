f9fcf89932adb5a235bc2c445f0df4c613cb361e
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-04-08 21:07:34 -0400

    adding the server package jsons and also adding the server js just need to switch it back to production once the app is ready to move to the client

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+// const routes = require("./routes");
+const { ApolloServer } = require("apollo-server-express");
+// the auth function for the apollo server
+const { authMiddleware } = require("./utils/auth");
+// import the schemas to the server
+const { typeDefs, resolvers } = require("./schemas");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  // this starts the apollo server
+  await server.start();
+  // applies apollo to the app itself
+  server.applyMiddleware({ app });
+
+  // once the server is up then this console log will help me get to the path to use graphql
+  console.log(
+    `Use graphql here: http://localhost:${PORT}${server.graphqlPath}`
+  );
+};
+// server is basically set up so just calling the function to run
+startServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+// this is the new way to use graphql's api route
+// server is good to go!
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+// commenting out the old use routes function because I am now using graphql instead of the restful api
+// app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
