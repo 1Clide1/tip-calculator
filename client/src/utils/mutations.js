56ddba09f24adf193b18989d3930a954ab95c3e5
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-04-11 20:06:46 -0400

    added the complete mutations for graphql which were all tested before adding to the client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import { gql } from "@apollo/client";
+// mutation to login a user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+// mutation to add a user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+// mutation to add tips that the user makes to the user
+export const ADD_TIP_HISTORY = gql`
+  mutation addTipHistory($tip: Int!) {
+    addTipHistory(tip: $tip) {
+      _id
+      username
+      tipHistory {
+        _id
+        tip
+      }
+    }
+  }
+`;
+// mutation to add percentage the user picks to the user
+export const ADD_PERCENTAGE = gql`
+  mutation addPercentage($percentage: String!) {
+    addPercentage(percentage: $percentage) {
+      _id
+      username
+      percentages {
+        _id
+        percentage
+      }
+    }
+  }
+`;
