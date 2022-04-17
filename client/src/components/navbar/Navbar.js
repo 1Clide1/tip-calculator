dbb4b98022ebcfe1deded1e946c6ddbd3b5632f5
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-04-17 17:08:14 -0400

    adding the navbar component will be improved in the future but it is right now functional

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,50 @@
+// import section
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { mainNavItems } from "./navItems";
+import Auth from "../../utils/auth";
+// import SignUpForm from "../../pages/SignupForm";
+// import LoginForm from "../../pages/LoginForm";
+import "./navbar.css";
+
+const Navbar = () => {
+  // setting the state of the mobile navbar menu
+  const [clicked, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!clicked);
+  };
+  return (
+    <>
+      <nav className="navbar-container">
+        {/* menu icon for the website */}
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={clicked ? "lni lni-cross-circle" : "lni lni-menu"}></i>
+        </div>
+        <ul className={clicked ? "nav-ul active" : "nav-ul"}>
+          {mainNavItems.map((item, index) => {
+            return (
+              // make sure li always has a key
+              <li key={index}>
+                <Link className={item.cName} to={item.url}>
+                  {item.text}
+                </Link>
+              </li>
+            );
+          })}
+          {Auth.loggedIn() ? (
+            <li>
+              <Link onClick={Auth.logout} className="navbar-links">
+                {" "}
+                Logout
+              </Link>
+            </li>
+          ) : (
+            <> </>
+          )}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
