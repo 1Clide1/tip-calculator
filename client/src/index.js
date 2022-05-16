2981a48758ec52ddf7bc45304ba3114a88397b08
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-05-16 19:19:48 -0400

    adding a service worker pushing to see if it works in production

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// serviceWorkerRegistration.register();
+if (process.env.NODE_ENV === "development") {
+  serviceWorkerRegistration.unregister();
+} else {
+  serviceWorkerRegistration.register();
+}
 ReactDOM.render(<App />, document.getElementById("root"));
+// run the new service worker
+// serviceWorker();
