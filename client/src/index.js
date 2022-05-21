eb001681c0ca63cd5cce6a7b1f6f8cfe9df2597f
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-05-21 15:45:22 -0400

    updated react dom render to the new version 18 create root

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
@@ -9,4 +10,7 @@ if (process.env.NODE_ENV === "development") {
 } else {
   serviceWorkerRegistration.register();
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
