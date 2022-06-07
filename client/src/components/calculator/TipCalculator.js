63b430205156503f95a763f0d34f1c5a620a1bbb
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-06-06 20:43:48 -0400

    I added a fix for the inputs that way they stop auto zooming in on mobile, also added the meta data to the home page instead of leaving it on the compenent level

--- client/src/components/calculator/TipCalculator.js
+++ client/src/components/calculator/TipCalculator.js
@@ -178,16 +178,6 @@ const TipCalculator = () => {
         itemScope
         itemType="http://schema.org/Article"
       >
-        {/* google search meta data better to have in the body of the page I am trying to target */}
-        <meta itemProp="name" content="Tip Calculator Web App" />
-        <meta
-          itemProp="description"
-          content="You ever need to figure out the tip on a night out? Well now you can with this cool tip calculator!"
-        />
-        <meta
-          itemProp="image"
-          content="http://keepdachange.app/favicon/android-chrome-512x512.png"
-        />
         <form className="tip-form-container" onSubmit={handleSubmit}>
           <label className="label-title"> Enter Your Bill Amount</label> <br />
           <input
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,21 @@ import "./home.css";
 import TipCalculator from "../components/calculator/TipCalculator";
 
 const Home = () => {
-  return <TipCalculator />;
+  return (
+    <>
+      {/* google search meta data better to have in the body of the page I am trying to target */}
+      <meta itemProp="name" content="Tip Calculator Web App" />
+      <meta
+        itemProp="description"
+        content="You ever need to figure out the tip on a night out? Well now you can with this cool tip calculator!"
+      />
+      <meta
+        itemProp="image"
+        content="http://keepdachange.app/favicon/android-chrome-512x512.png"
+      />
+      <TipCalculator />
+    </>
+  );
 };
 
 export default Home;
