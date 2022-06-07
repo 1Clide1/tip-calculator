e1a91ff4d0048d8f20aeb3f23c4aa567ea220f58
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-06-06 22:24:14 -0400

    reverted to react 17 because of react loader spinner npm

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
@@ -10,7 +9,7 @@ if (process.env.NODE_ENV === "development") {
 } else {
   serviceWorkerRegistration.register();
 }
-// ReactDOM.render(<App />, document.getElementById("root"));
-const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+ReactDOM.render(<App />, document.getElementById("root"));
+// const container = document.getElementById("root");
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
