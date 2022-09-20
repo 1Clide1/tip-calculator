93d624b83bc517f46e4491e0e9302f2010b31dd5
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-09-19 20:24:27 -0400

    added a material ui to use their switch component, added the switch to the site. going back to the switch in the future to override the style fully

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import { mainNavItems } from "./navItems";
 import Auth from "../../utils/auth";
 import { themeSelector } from "../../App";
+// importing the material ui switch and the styled component to properly style the toggle switch
+import { Switch, SwitchProps } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import "./navbar.css";
 
 const Navbar = () => {
@@ -41,7 +44,67 @@ const Navbar = () => {
   });
 
   // getting switch theme from the theme selector context that I have created
+
   const { SwitchTheme } = useContext(themeSelector);
+
+  // material ui theme toggle switch customization
+  // **coming back to this because i do not fully understand how to properly override the switch**
+
+  // const ThemeToggleSwitch = styled((props: SwitchProps) => (
+  //   <Switch
+  //     focusVisibleClassName=".Mui-focusVisible"
+  //     disableRipple
+  //     {...props}
+  //   />
+  // ))(({ theme }) => ({
+  //   width: 42,
+  //   height: 26,
+  //   padding: 0,
+  //   "& .MuiSwitch-switchBase": {
+  //     padding: 0,
+  //     margin: 2,
+  //     transitionDuration: "300ms",
+  //     "&.Mui-checked": {
+  //       transform: "translateX(16px)",
+  //       color: "#fff",
+  //       "& + .MuiSwitch-track": {
+  //         backgroundColor:
+  //           theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
+  //         opacity: 1,
+  //         border: 0,
+  //       },
+  //       "&.Mui-disabled + .MuiSwitch-track": {
+  //         opacity: 0.5,
+  //       },
+  //     },
+  //     "&.Mui-focusVisible .MuiSwitch-thumb": {
+  //       color: "#33cf4d",
+  //       border: "6px solid #fff",
+  //     },
+  //     "&.Mui-disabled .MuiSwitch-thumb": {
+  //       color:
+  //         theme.palette.mode === "light"
+  //           ? theme.palette.grey[100]
+  //           : theme.palette.grey[600],
+  //     },
+  //     "&.Mui-disabled + .MuiSwitch-track": {
+  //       opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+  //     },
+  //   },
+  //   "& .MuiSwitch-thumb": {
+  //     boxSizing: "border-box",
+  //     width: 22,
+  //     height: 22,
+  //   },
+  //   "& .MuiSwitch-track": {
+  //     borderRadius: 26 / 2,
+  //     backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
+  //     opacity: 1,
+  //     transition: theme.transitions.create(["background-color"], {
+  //       duration: 500,
+  //     }),
+  //   },
+  // }));
 
   return (
     <>
@@ -71,9 +134,11 @@ const Navbar = () => {
           ) : (
             <> </>
           )}
-          <button className="percent-btn" onClick={SwitchTheme}>
-            Switch Theme
-          </button>
+          <Switch
+            className="theme-switch-btn"
+            onClick={SwitchTheme}
+            color="secondary"
+          />
         </ul>
       </nav>
     </>
