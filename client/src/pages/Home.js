2d8435c79e3ef8c28ed7bde71811bee0e8ffa80e
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-11-06 17:46:42 -0500

    added some comments to the index.html, made it easier to read and properly added schema.org meta data to home.js to get a better seo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,14 +7,21 @@ const Home = () => {
   return (
     <>
       {/* google search meta data  */}
-      <meta itemProp="name" content="Tip Calculator Web App" />
       <meta
+        itemscope
+        itemtype="https://schema.org"
         itemProp="description"
         content="You ever need to figure out the tip on a night out? Well now you can with this cool tip calculator!"
       />
       <meta
+        itemscope
+        itemtype="https://schema.org"
         itemProp="image"
         content="http://keepdachange.app/favicon/android-chrome-512x512.png"
+      />
+      <meta
+        name="description"
+        content="You ever need to figure out the tip on a night out? Well now you can with this cool tip calculator!"
       />
       <TipCalculator />
     </>
