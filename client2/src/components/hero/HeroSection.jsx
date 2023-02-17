c5fc63478385e20b7b55c598eb9fe07a2a232ab8
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-02-16 22:29:25 -0500

    transfering over the navbar code to the hero section added the imports, clicked state, and a function to handle that state

--- client2/src/components/hero/HeroSection.jsx
+++ client2/src/components/hero/HeroSection.jsx
@@ -1,13 +1,21 @@
 // imports
-import React from 'react';
-import '../styles/partials/_hero.scss';
+import React, { useState, useEffect, useRef, useContext } from 'react';
+// react router dom
+import { Link } from 'react-router-dom';
+// import from utils
+import { mainNavItems } from '../../utils/navItems';
+import auth from '../../utils/auth';
+import { themeSelector } from '../../../../client/src/App';
 
-const HeroSection = () => {
-  return (
-    <header className='header'>
-      <h1 className='header-title'>Webpack Starter Pack</h1>
-    </header>
-  );
-};
+function HeroSection() {
+  // using state to see if nav is clicked
+  const [clicked, setClicked] = useState(false);
+
+  // function to set clicked to true if clicked
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+  return <header className='header'></header>;
+}
 
 export default HeroSection;
