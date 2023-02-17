3208694c4d5eb4e0ea9bc64a361c7491d4f1f161
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-02-17 00:26:58 -0500

    refactored my custom hook to the best of my abilites will change if for whatever reason it doesnt work also added some short comments for readibility

--- client2/src/components/hero/HeroSection.jsx
+++ client2/src/components/hero/HeroSection.jsx
@@ -15,6 +15,25 @@ function HeroSection() {
   const handleClick = () => {
     setClicked(!clicked);
   };
+
+  // function to set clicked back to false to close the menu
+  const closeMenu = () => {
+    setClick(!clicked);
+  };
+
+  // custom hook to close the mobile nav menu by clicking away from the menu
+  const useClickOutside = () => {
+    // creating a reference
+    let domNode = useRef();
+
+    useEffect(() => {
+      let handler = (e) => {
+        !domNode.current?.contains(e.target) ? closeMenu() : null; //if the click happens within the elements dimensions then nothing will happen else it will close the menu
+      };
+      document.addEventListener('mousedown', handler);
+      return document.removeEventListener('mousedown', handler); //remove the return function because i believe this as is should still work
+    });
+  };
   return <header className='header'></header>;
 }
 
