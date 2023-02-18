d851ccd6bcf94f1cf9ffb71889cb86499f1c8d6b
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-02-17 22:16:58 -0500

    got rid of the runtime:automatic on @babel/preset-react might add it back if it does not cause any errors

--- client2/babel.config.js
+++ client2/babel.config.js
@@ -14,7 +14,7 @@ module.exports = {
   presets: [
     // Runtime automatic with React 17+ allows not importing React
     // in files only using JSX (no state or React methods)
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-react',
     '@babel/preset-env',
   ],
   plugins,
