1ddc251210794aca1126e1c2d104649e8dd0ecbf
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-03-04 13:50:23 -0500

    updated the tip calculator jsx and the added service worker imports to the index jsx

--- client2/src/components/calculator/TipCalculator.jsx
+++ client2/src/components/calculator/TipCalculator.jsx
@@ -8,15 +8,30 @@ import { ADD_PERCENTAGE, ADD_TIP_HISTORY } from '../../utils/mutations';
 // syling
 import '../../styles/partials/_tipcalculator.scss';
 
-function TipCalculator(){
-    // State Code :
-    const [submit, setSumbit] = useState(false); // state for handling form submissions
+function TipCalculator() {
+  // State Code :
+  const [submit, setSumbit] = useState(false); // state for handling form submissions
 
-    const [form, setForm]= useState({ // state to manage the user input fields
-        bill: '',
-        groupSize: '', // was originally groupNum but this should be more readable
-    });
-    
-};
+  // state to manage the user input fields
+  const [form, setForm] = useState({
+    bill: '',
+    groupSize: '', // was originally groupNum but this should be more readable
+  });
+// state to manage percent button values in the form
+  const [percentage, setPercentage] = useState({
+    percentValue: '', //just value seemed too vague so this should be better for readablility
+  });
+
+//   state to save the tips and the tip totals
+const [tipAmounts, setTipAmounts]= useState({
+    tip: '',
+    total: '',
+    groupTip: '',
+    groupTotal:'',
+});
+
+const [group, setGroup] =useState(false);
+
+}
 
 export default TipCalculator;
--- client2/src/index.jsx
+++ client2/src/index.jsx
@@ -4,6 +4,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
 
+//possible service worker activation code
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// serviceWorkerRegistration.register();
+// if (process.env.NODE_ENV === 'development') {
+//   serviceWorkerRegistration.unregister();
+// } else {
+//   serviceWorkerRegistration.register();
+// }
+
 // creating the root
 const container = document.getElementById('root');
 const root = createRoot(container);
