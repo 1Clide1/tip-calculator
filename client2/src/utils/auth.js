1fe34b3fcda23455745eeb390d52f75a3a54e66d
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-04-20 23:33:18 -0400

    eslint threw an error and expected this to be used in the functions so i added this to what i thought would be appropriate. not sure if this was the proper way to resolve the eslint errors but it worked

--- client2/src/utils/auth.js
+++ client2/src/utils/auth.js
@@ -18,10 +18,12 @@ class AuthService {
   // check if token is expired
   isTokenExpired(token) {
     try {
-      const decoded = decode(token);
+      const decoded = this.decode(token);
+      // if the decoded token is expired then return true
       if (decoded.exp < Date.now() / 1000) {
         return true;
-      } else return false;
+      }
+      return false; // if token is not expired yet then auto return false
     } catch (err) {
       return false;
     }
@@ -29,18 +31,18 @@ class AuthService {
 
   getToken() {
     // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
+    return this.localStorage.getItem('id_token');
   }
 
   login(idToken) {
     // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
+    this.localStorage.setItem('id_token', idToken); // not sure if optional chaining this is the right answer but also not sure what my other options are
     window.location.assign('/');
   }
 
   logout() {
     // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
+    this.localStorage.removeItem('id_token');
     // this will reload the page and reset the state of the application
     window.location.assign('/');
   }
