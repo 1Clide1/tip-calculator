efa7f92607a4d2d66187e177d543c57c7b984de1
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-07-31 02:04:16 -0400

    I fixed the errors and refatored the code accordingly

--- client/src/pages/SignupForm.jsx
+++ client/src/pages/SignupForm.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
-// import css
-import './signup-login.css';
 
-const SignupForm = () => {
+// import style
+import '../styles/partials/_signup-login.scss';
+
+function SignupForm() {
   // set initial form state
   const [userFormData, setUserFormData] = useState({
     username: '',
@@ -27,79 +28,80 @@ const SignupForm = () => {
       const { data } = await addUser({ variables: { ...userFormData } });
       Auth.login(data.addUser.token);
       console.log(data.addUser.token);
-    } catch (e) {
-      console.error(e);
+      //  e was already used on line 24 so I specified this var to err instead
+    } catch (err) {
+      console.error(err);
     }
   };
 
   return (
     <>
       <div className='form-container'>
-        <label title='Sign-Up Form' className='form-header'>
+        <label htmlFor='sign-up' title='Sign-Up Form' className='form-header'>
           Sign-Up
+          <form id='sign-up' onSubmit={handleFormSubmit}>
+            <label
+              htmlFor='username'
+              title='Your Username'
+              className='form-label'
+            >
+              Username:
+              <input
+                type='text'
+                id='username'
+                name='username'
+                placeholder='enter your username'
+                className='form-input'
+                value={userFormData.username}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label htmlFor='email' title='Your Email' className='form-label'>
+              Email:
+              <input
+                type='email'
+                id='email'
+                name='email'
+                placeholder='enter your email (example@example.com)'
+                className='form-input'
+                value={userFormData.email}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label
+              htmlFor='password'
+              title='Your Password'
+              className='form-label'
+            >
+              Password:
+              <input
+                type='password'
+                id='password'
+                name='password'
+                placeholder='enter your password'
+                className='form-input'
+                value={userFormData.password}
+                onChange={handleInputChange}
+              />
+            </label>
+            <button type='submit' className='submit-btn'>
+              {' '}
+              Sign-Up Here
+            </button>
+          </form>
         </label>
-        <form onSubmit={handleFormSubmit}>
-          <label
-            htmlFor='username'
-            title='Your Username'
-            className='form-label'
-          >
-            Username:
-          </label>
-          <input
-            type='text'
-            id='username'
-            name='username'
-            placeholder='enter your username'
-            className='form-input'
-            value={userFormData.username}
-            onChange={handleInputChange}
-          />
-          <label htmlFor='email' title='Your Email' className='form-label'>
-            Email:
-          </label>
-          <input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='enter your email (example@example.com)'
-            className='form-input'
-            value={userFormData.email}
-            onChange={handleInputChange}
-          />
-          <label
-            htmlFor='password'
-            title='Your Password'
-            className='form-label'
-          >
-            Password:
-          </label>
-          <input
-            type='password'
-            id='password'
-            name='password'
-            placeholder='enter your password'
-            className='form-input'
-            value={userFormData.password}
-            onChange={handleInputChange}
-          />
-          <button type='submit' className='submit-btn'>
-            {' '}
-            Sign-Up Here
-          </button>
-        </form>
       </div>
       {/* if the sign up has an error it will display this instead */}
       {error && (
         <div className='error-container'>
-          <i className='lni lni-sad error-icon'></i>
+          <i className='lni lni-sad error-icon' />
           <p className='error-title'>
-            Oops, sign-up didn't work, might want to try it again.
+            Oops, sign-up didnt work, might want to try it again.
           </p>
         </div>
       )}
     </>
   );
-};
+}
 
 export default SignupForm;
